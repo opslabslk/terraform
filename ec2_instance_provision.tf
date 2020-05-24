661a80e0731b63ded9d3fcbc5be5e09c046655a6
Pubudu Senadeera <Pubudu_105198@dialog.dialoggsm.com>
2020-05-24 08:42:02 +0530

    initial commit

---
+++ ec2_instance_provision.tf
@@ -0,0 +1,7 @@
+# resource "aws_instance" "name" {
+#   ami             = "ami-0f7919c33c90f5b58"
+#   instance_type   = "t2.micro"
+#   security_groups = ["${aws_security_group.allow_http.name}"]
+#   key_name        = "awstest"
+# }
+
